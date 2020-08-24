68aeb580207bc148f25fc379be85827281582224
Vedvardhan Gyanmote <vedvardhan.g@gmail.com>
2020-08-24 15:28:26 +0530

    Added a customizable button for changing background color

--- public/chat.js
+++ public/chat.js
@@ -1,12 +1,12 @@
 // Make Connection
-var socket = io.connect("http://localhost:3000");
+let socket = io.connect("http://localhost:3000");
 
 // Query DOM
-var handle = prompt("Enter your nickname");
-var message = document.getElementById('message');
-var btn = document.getElementById('send');
-var output = document.getElementById('output');
-var feedback = document.getElementById('feedback')
+let handle = prompt("Enter your nickname");
+let message = document.getElementById('message');
+let btn = document.getElementById('send');
+let output = document.getElementById('output');
+let feedback = document.getElementById('feedback')
 
 // Logic if handle name is empty
 if (handle == "") {
@@ -37,4 +37,15 @@ socket.on('chat', (data) => {
 
 socket.on('typing', (data) => {
     feedback.innerHTML = `<p><em>${data} is typing...</em></p>`
-})
+});
+
+
+// function for customising background
+
+function customise() {
+    let status = prompt("Click any key and confirm");
+    if(status) {
+        let choice = prompt("Please type a VALID color or a VALID hex code(rgb values are not considered)")
+        document.bgColor = choice
+    }
+}
